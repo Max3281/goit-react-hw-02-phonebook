4f009b6e4374d232eb5c213a21fef17a30d5414f
Max3281 <113448092+Max3281@users.noreply.github.com>
2023-04-15 21:11:43 +0300

    validadd

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -39,6 +39,9 @@ class ContactsForm extends Component {
           placeholder="Full name"
           value={name}
           onChange={this.handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
         />
         <label htmlFor="number">Tel.Number</label>
         <input
@@ -47,6 +50,9 @@ class ContactsForm extends Component {
           placeholder="Tel. number"
           value={number}
           onChange={this.handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
         />
 
         <button type="submit">Add contact</button>
